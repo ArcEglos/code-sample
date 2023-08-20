ef07b1e730c6cb64ba483dcf0745e6c0de89c329
Florian Scholz <scholz@heartbeat-med.de>
2023-08-20 21:11:26 +0200

    Added dummy code-splitting implementation

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,24 +1,33 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Flex, Text } from "@chakra-ui/react";
-
-import Launches from "./launches";
-import Launch from "./launch";
-import Home from "./home";
-import LaunchPads from "./launch-pads";
-import LaunchPad from "./launch-pad";
+import { Flex, Spinner, Text } from "@chakra-ui/react";
 import { Favourites } from "./favourites";
+
+const Launches = React.lazy(() => import("./launches"));
+const Launch = React.lazy(() => import("./launch"));
+const Home = React.lazy(() => import("./home"));
+const LaunchPads = React.lazy(() => import("./launch-pads"));
+const LaunchPad = React.lazy(() => import("./launch-pad"));
 
 export default function App() {
   return (
     <div>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/launches" element={<Launches />} />
-        <Route path="/launches/:launchId" element={<Launch />} />
-        <Route path="/launch-pads" element={<LaunchPads />} />
-        <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
-      </Routes>
+      <React.Suspense
+        fallback={
+          <Flex justifyContent="center" alignItems="center" minHeight="50vh">
+            <Spinner size="lg" />
+          </Flex>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/launches" element={<Launches />} />
+          <Route path="/launches/:launchId" element={<Launch />} />
+          <Route path="/launch-pads" element={<LaunchPads />} />
+          <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
+        </Routes>
+      </React.Suspense>
     </div>
   );
 }
